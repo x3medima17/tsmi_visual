3ff224a0ff8ec062a1c7cc3ee5cd8ca6778a8df4
Dumitru Savva <dimasavva17@gmail.com>
2016-12-06 22:56:54 +0900

    gzip

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ class Application(tornado.web.Application):
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             cookie_secret="Settings.COOKIE_SECRET",
+            compress_response=True
         )
         tornado.web.Application.__init__(self, handlers, **settings)
 

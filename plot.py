886c069d4c28d0ac404b36cafb5187676f190d29
Your Name <you@example.com>
2017-02-08 01:51:28 +0300

    statbuilder

--- plot.py
+++ plot.py
@@ -11,90 +11,312 @@ from collections import Counter
 import os
 import shutil
 import gridfs
+import fractions
 import matplotlib as mpl
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 15, 10
+
+
 mpl.use('Agg')
 
 
-client = pymongo.MongoClient()
+client = pymongo.MongoClient("128.199.148.157")
+
 db = client.tsmi
+
+res = db.authenticate('tsmi', '157359')
 fs = gridfs.GridFS(db)
 
+class StatsBuilder(object):
+	def __init__(self, id):
+		self.id = id
+		self.oid = ObjectId(id)
+		self.item = db.runs.find_one({"_id" : self.oid})
+		if not self.item:
+			print("Fetch failed")
 
+		self.figures = dict()
+
+	def plot_main(self):	
+
+		deltas = self.item["data"]["delta"]
+
+		fig = plt.figure()
+		fig.canvas.set_window_title("Main")
+
+		ax = plt.subplot(121)
+		ax.plot(range(1,len(deltas)+1), deltas)
+		
+		ax.set_xlabel("Iteration")
+		ax.set_ylabel("Value")
+		ax.set_yscale('linear')
+		ax.set_title('Delta')
+		ax.grid(True)
+
+
+		ax = plt.subplot(122)
+
+		ax.hist(deltas, 50)# bins=np.arange(min(deltas), max(deltas), 0.1))
+		ax.set_title('Delta hist')
+
+		ax.set_xlabel("Value")
+		ax.set_ylabel("Occurence")
+		ax.grid(True)
+
+		self.figures["main"] = [fig]
+		# plt.savefig("{}/main.png".format(path))       
+
+	def get_formation_dict(self):
+		""" 
+			Example
+			{ 
+				'1' : {
+						'k_perm' : <index>,
+						'p_i_red': <index>
+					  },
+			}
+		"""
+		formations = {}
+		info = self.item["meta"]["param_info"]
+
+		for i, item in enumerate(info):
+			if not item[0] in formations.keys():
+				formations[item[0]] = dict()
+			formations[item[0]][item[1]] = i
+		return formations
+
+	def plot_paths(self):
+		formations = self.get_formation_dict()
+
+		figs = []
+		item = self.item
+		
+		for key, value in formations.iteritems():
+			fig = plt.figure()
+			ax = plt.subplot(111)
+			x = item["data"]["positions"][value["k_perm"]]
+			y = item["data"]["positions"][value["p_i_red"]]
+			ax.plot(x, y, "ro")# bins=np.arange(min(deltas), max(deltas), 0.1))
+			ax.set_title('Formation {} path'.format(key))
+			fig.canvas.set_window_title('Formation {} path'.format(key))
+			ax.grid(True)
+
+			ax.set_xlabel('k_perm')
+			ax.set_ylabel('p_i_red')
+
+
+			figs.append(fig)
+    
+		self.figures["paths"] = figs
+
+
+	def plot_positions(self):
+		figs = []
+		item = self.item
+		N = len(item["data"]["positions"])
+		assert N%2 == 0
+		qq = 0
+		for i in range(N/2):
+			qq += 1
+			x = item["data"]["positions"][2*i]
+			y = item["data"]["positions"][2*i+1]
+
+			fig = plt.figure()
+			plt.subplot(121)
+			plt.plot(range(1,len(x)+1), x)
+			plt.title('Param {}'.format((qq-1)*2 +1))
+			plt.grid(True)
+
+			plt.subplot(122)
+			plt.plot(range(1,len(y)+1), y)
+			plt.title('Param {}'.format((qq-1)*2 +2))
+			plt.grid(True)
+			  
+			figs.append(fig)
+
+			# plt.savefig("{}/positions{}.png".format(path,qq))       
+		self.figures["positions"] = figs
+
+
+	def plot_freq(self, bins=10):	
+
+		formations = self.get_formation_dict()
+
+		figs = []
+		item = self.item
+		
+		for key, value in formations.iteritems():
+			fig = plt.figure()
+			ax = plt.subplot(111)
+			x = item["data"]["positions"][value["k_perm"]]
+			y = item["data"]["positions"][value["p_i_red"]]
+
+			plt.hist2d(x, y, bins=bins, norm=LogNorm())
+			plt.colorbar()
+
+			ax.set_title('Formation {} frequency'.format(key))
+			fig.canvas.set_window_title('Formation {} frequency'.format(key))
+			ax.grid(True)
+
+			ax.set_xlabel('k_perm')
+			ax.set_ylabel('p_i_red')
+
+			figs.append(fig)
+
+		self.figures["frequ"] = figs
+
+	def plot_accepted_stats(self):
+		item = self.item
+		N = len(item["data"]["positions"])
+
+		accepted = np.array(item["data"]["accepted"])
+		param_indexes = np.array(item["data"]["param_index"]) + 1
+
+		accepted_only = filter(lambda x: x != 0, param_indexes * accepted)
+		dropped_only = filter(lambda x: x != 0, param_indexes * ((accepted - 1) * -1))
+
+		count_accepted = dict(Counter(accepted_only))
+		count_dropped = dict(Counter(dropped_only))
+		for i in range(1, N+1):
+			if i not in count_accepted.keys():
+				count_accepted[i] = 0
+			if i not in count_dropped.keys():
+				count_dropped[i] = 0
+				
+		accepted = count_accepted.values()
+		pprint(count_accepted)
+		ind = np.arange(N)  # the x locations for the groups
+		width = 0.35       # the width of the bars
+
+		fig, ax = plt.subplots()
+		
+		rects1 = ax.bar(ind, accepted, width, color='y')
+
+		dropped = count_dropped.values()
+		rects2 = ax.bar(ind + width, dropped, width, color='r')
+
+		# add some text for labels, title and axes ticks
+		ax.set_ylabel('Points')
+		ax.set_title('Accepted and dropped points by parameter')
+		# ax.set_xticks(ind + width / 2)
+		info = self.item["meta"]["param_info"]
+		labels = []
+		for item in info:
+			labels.append("Frm {} {}".format(item[0], item[1]))
+			labels.append("")
+		ax.set_xticklabels(labels, ha='left')
+		# fig.subplots_adjust(hspace=-0.5)
+
+		ax.legend((rects1[0], rects2[0]), ('Accepted', 'Dropped'))
+
+
+		def autolabel(rects):
+		    """
+		    Attach a text label above each bar displaying its height
+		    """
+		    for rect in rects:
+		        height = rect.get_height()
+		        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+		                '%d' % int(height),
+		                ha='center', va='bottom')
+
+		autolabel(rects1)
+		autolabel(rects2)
+
+		self.figures["accepted_stats"] = [fig]
+		# sys.exit() 
+		# plt.savefig("{}/accepted.png".format(path))       
+
+	@staticmethod
+	def factorize2(n):
+		sol = [1, n]
+		for i in range(1,int(np.sqrt(n)+1)):
+			if n%i == 0 and (i - n/i)**2 < (sol[0] - sol[1])**2:
+				sol = [i, n/i]
+		return sol 
+
+	def plot_positions_hists(self):
+		N = len(self.item["data"]["positions"])
+		print(N)
+		w,h = self.factorize2(N)
+
+		fig = plt.figure()
+		qq = 0
+		for i in range(1, w+1):
+			for j in range(1, h+1):
+				vals = self.item["data"]["positions"][qq]
+				info = self.item["meta"]["param_info"][qq]
+				qq+=1
+				plt.subplot(w*100 + h*10 + qq)
+				plt.hist(vals, 50)# bins=np.arange(min(deltas), max(deltas), 0.1))
+				plt.title('Formation {} {} hist'.format(info[0], info[1]))
+				plt.grid(True)
+		self.figures["positions_hists"] = fig
+
+	def plot_field_line(self, field):
+		fig = plt.figure()
+		ax = plt.subplot(111)
+		data = self.item["data"][field]
+		plt.plot(range(1,len(data)+1), data)
+		ax.set_xlabel("Iteration")
+		ax.set_ylabel(field)
+
+		ax.set_title(field)
+		plt.grid(True)
+
+	def plot_field_hist(self, field, bins=20):
+		fig = plt.figure()
+		plt.subplot(111)
+		data = self.item["data"][field]
+		plt.hist(data,bins)
+		plt.title(field)
+		plt.grid(True)
+
+
+	def plot_all(self):
+		self.plot_main()
+		self.plot_paths()
+		self.plot_positions()
+		self.plot_freq()
+		self.plot_accepted_stats()
+
+
+	def show(self):
+		plt.show();
+
+	def insert(self):
+		figs = self.figures
+
+		self.path = "/tmp/tsmi/{}".format(self.id)
+		oid = ObjectId(self.id)
+		path = self.path
+		try:
+			shutil.rmtree(path)
+		except:
+			print(sys.exc_info()[0])
+			pass
+
+		os.makedirs(self.path)
+		pprint(figs)
+		for key, value in figs.iteritems():
+			for i,item in enumerate(value):
+				item.savefig("{}/{}{}.png".format(path,key,i))
+		# figs["main"].savefig("/tmp/main.png")       
+
+
+		shutil.make_archive("/tmp/tsmi/out", 'zip', path)
+		zip = open("/tmp/tsmi/out.zip", "rb").read()
+		with fs.new_file(filename="{}.zip".format(oid)) as fb:
+			fb.write(zip)
+
+		plt.clf()
+		shutil.rmtree(path)
+		os.remove("/tmp/tsmi/out.zip")
+	
 
 def build_stats(oid):
-	path = "/tmp/tsmi/{}".format(oid)
-
-	try:
-		shutil.rmtree(path)
-	except:
-		pass
-
-	os.makedirs(path)
-	id = ObjectId(oid)
-
-
-
-	item = db.runs.find_one({"_id" : id})
-	# pprint(item["data"].keys())	
-	deltas = item["data"]["delta"]
-
-	plt.figure()
-	plt.subplot(121)
-	plt.plot(range(1,len(deltas)+1), deltas)
-	plt.yscale('linear')
-	plt.title('Delta')
-	plt.grid(True)
-
-
-
-	plt.subplot(122)
-
-	plt.hist(deltas, 50)# bins=np.arange(min(deltas), max(deltas), 0.1))
-	plt.title('Delta hist')
-	plt.grid(True)
-
-	plt.savefig("{}/main.png".format(path))       
-
-
-	N = len(item["data"]["positions"])
-	assert N%2 == 0
-	qq = 0
-	for i in range(N/2):
-		qq += 1
-		plt.figure()
-		plt.subplot(111)
-
-		x = item["data"]["positions"][2*i]
-		y = item["data"]["positions"][2*i+1]
-
-		plt.plot(x, y, "ro")# bins=np.arange(min(deltas), max(deltas), 0.1))
-		plt.title('Path '+ str(qq))
-		plt.grid(True)
-		plt.savefig("{}/path{}.png".format(path, qq))    
-	       
-
-
-		plt.figure()
-		plt.subplot(121)
-		plt.plot(range(1,len(x)+1), x)
-		plt.title('Param {}'.format((qq-1)*2 +1))
-		plt.grid(True)
-
-		plt.subplot(122)
-		plt.plot(range(1,len(y)+1), y)
-		plt.title('Param {}'.format((qq-1)*2 +2))
-		plt.grid(True)
-	       
-
-		plt.savefig("{}/positions{}.png".format(path,qq))       
-
-
-		plt.figure()
-		plt.subplot(111)
-		plt.hist2d(x, y, bins=100, norm=LogNorm())
-		plt.colorbar()
-
-		plt.savefig("{}/shoot_freq{}.png".format(path,qq))       
+	
 
 
 
@@ -131,65 +353,6 @@ def build_stats(oid):
 	#                               interval=10, blit=True, repeat=False)
 
 
-	N = len(item["data"]["positions"])
-
-	accepted = np.array(item["data"]["accepted"])
-	param_indexes = np.array(item["data"]["param_index"]) + 1
-
-	accepted_only = filter(lambda x: x != 0, param_indexes * accepted)
-	dropped_only = filter(lambda x: x != 0, param_indexes * ((accepted - 1) * -1))
-
-	count_accepted = dict(Counter(accepted_only))
-	count_dropped = dict(Counter(dropped_only))
-	for i in range(1, N+1):
-		if i not in count_accepted.keys():
-			count_accepted[i] = 0
-		if i not in count_dropped.keys():
-			count_dropped[i] = 0
-			
-	# print(dropped_only, accepted_only,accepted)
-
-	# print(count_accepted, count_dropped)
-	# print(len(item["data"]["accepted"]), sum(count_accepted.values()) + sum(count_dropped.values()))
-	# N = len(count_accepted.keys())
-	# N = 5
-	accepted = count_accepted.values()
-	pprint(count_accepted)
-	ind = np.arange(N)  # the x locations for the groups
-	width = 0.35       # the width of the bars
-
-	fig, ax = plt.subplots()
-	print(ind, accepted)
-	rects1 = ax.bar(ind, accepted, width, color='r')
-
-	dropped = count_dropped.values()
-	print(ind+width, dropped, width)
-	rects2 = ax.bar(ind + width, dropped, width, color='y')
-
-	# add some text for labels, title and axes ticks
-	ax.set_ylabel('Points')
-	ax.set_title('Accepted and dropped points by parameter')
-	ax.set_xticks(ind + width / 2)
-	ax.set_xticklabels(["Param {}".format(x) for x in range(1,N+1)])
-
-	ax.legend((rects1[0], rects2[0]), ('Accepted', 'Dropped'))
-
-
-	def autolabel(rects):
-	    """
-	    Attach a text label above each bar displaying its height
-	    """
-	    for rect in rects:
-	        height = rect.get_height()
-	        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-	                '%d' % int(height),
-	                ha='center', va='bottom')
-
-	autolabel(rects1)
-	autolabel(rects2)
-
-	# sys.exit()
-	plt.savefig("{}/accepted.png".format(path))       
 	
 	shutil.make_archive("/tmp/tsmi/out".format(path), 'zip', path)
 	zip = open("/tmp/tsmi/out.zip", "rb").read()
@@ -201,9 +364,9 @@ def build_stats(oid):
 	os.remove("/tmp/tsmi/out.zip")
 	# plt.show()
 
+	
 if __name__ == "__main__":
-	build_stats("5876b835f74786316bd909d9")
-	sys.exit()
+
 	lst = list(db.runs.find())
 	print len(lst)
 	i = 0
@@ -211,5 +374,28 @@ if __name__ == "__main__":
 		i+=1
 		print(i)
 		oid = str(item["_id"])
-		print(oid)
-		build_stats(oid)
+		try:
+			obj = StatsBuilder("5894455af7478630fb2f04b9")
+			obj.plot_all()
+			obj.insert()
+		except:
+			print("Failed")
+		
+	# info = obj.item["meta"]["param_info"]
+
+	# print(info)
+	# obj.plot_main()
+	# obj.plot_positions_hists()
+	# obj.plot_paths()
+	# obj.plot_freq()
+
+	# obj.plot_accepted_stats()
+
+	# obj.plot_field_line("delta")
+	# obj.plot_field_hist("acceptance_probability")
+	# obj.plot_field_hist("random_probability")
+	# obj.show()
+
+	sys.exit()
+	# build_stats("5876b835f74786316bd909d9")
+	

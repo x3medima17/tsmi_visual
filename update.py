210fcacd07a54b1c2e4fed584c45bbc54584bc25
Dumitru Savva <dimasavva17@gmail.com>
2017-02-22 12:01:32 +0300

    filter next

--- update.py
+++ update.py
@@ -2,8 +2,8 @@ import plot
 import sys
 
 def filt(item):
-    item = filter(lambda x: item["data"]["iters"] > 50, item)
-    return item
+    return filter(lambda x: item["data"]["iters"] > 50, item).next()
+
 
 if len(sys.argv) == 2:
     plot.StatsBuilder.update(sys.argv[1], [filt])

58279a0cd0b84f569faa922c31a631e4f62aa15b
Dumitru Savva <dimasavva17@gmail.com>
2017-02-22 12:03:46 +0300

    filter next

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import plot
 import sys
 
 def filt(item):
-    return filter(lambda x: item["data"]["iters"] > 50, item).next()
+    return list(filter(lambda x: item["data"]["iters"] > 50, item))[0]
 
 
 if len(sys.argv) == 2:

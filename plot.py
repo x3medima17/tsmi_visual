2c17f28a2db2b5c0908d85be4b99c1d96435e98a
Dumitru Savva <dimasavva17@gmail.com>
2017-02-22 12:56:49 +0300

    reduce accepted

--- plot.py
+++ plot.py
@@ -58,7 +58,7 @@ class StatsBuilder(object):
 
         accepted = reduce(lambda x,y : x & y, accepted)
         for key, value in self.item["data"].items():
-            _, self.item[key] = zip(*[(i,x) for i,x in enumerate(self.item[key]) if i in accepted])
+            _, self.item["data"][key] = zip(*[(i,x) for i,x in enumerate(self.item["data"][key]) if i in accepted])
         print(accepted)
         # print(self.item)
         # print(self.item["data"]["iters"])

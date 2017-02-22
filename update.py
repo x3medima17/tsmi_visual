af2422b910fd38f0bb6b4e477c169994159a4c5b
Dumitru Savva <dimasavva17@gmail.com>
2017-02-22 13:50:11 +0300

    2 filters

--- update.py
+++ update.py
@@ -1,14 +1,21 @@
 import plot
 import sys
 
-def filt(item):
+def filt_iter(item):
     s = set()
     for i, item in enumerate(item["data"]["iters"]):
         if  item > 5:
             s |= {i}
     return s
 
+def filt_delta(item):
+    s = set()
+    for i, item in enumerate(item["data"]["delta"]):
+        if item < 0.000035:
+            s |= {i}
+    return s
+
 if len(sys.argv) == 2:
-    plot.StatsBuilder.update(sys.argv[1], [filt])
+    plot.StatsBuilder.update(sys.argv[1], [filt_iter, filt_delta])
 else:
     plot.StatsBuilder.update()
